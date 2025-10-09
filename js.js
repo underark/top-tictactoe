81e6204be0af5a3937d1ed1ddf787b201eeb5f17
alex <peter.alex.morris@gmail.com>
2025-10-09 13:54:36 +0900

    Added function to game board
    Can now mark a position on the board as occupied

--- js.js
+++ js.js
@@ -6,20 +6,26 @@ const gameBoard = (function () {
 	const boardSize = 9;
 	const board = new Array(boardSize);
 
-	const showBoard = () => {
-		console.log(board);
-	}
-
 	const setBoard = () => {
 		for (let i = 0; i < boardSize; i++) {
 			board[i] = false;
 		}
 	}
 
+	const showBoard = () => {
+		console.log(board);
+	}
+
+	const markBoard = (position) => {
+		if (position >= 0 && position <= boardSize) {
+			board[position] = true;
+		}
+	}
+
 	setBoard();
 	return {
 		showBoard,
+		markBoard,
 	}
 })();
 
-gameBoard.showBoard();

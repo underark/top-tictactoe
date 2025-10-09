3381bd76a012fb8d8a90bb999df03f20affbb6d8
alex <peter.alex.morris@gmail.com>
2025-10-09 13:48:20 +0900

    Created game board object
    Used module pattern and function factory to create a game board that prints to the console

--- js.js
+++ js.js
@@ -1,1 +1,25 @@
-console.log("Hello");
+function createPlayer(name) {
+	return {name};
+}
+
+const gameBoard = (function () {
+	const boardSize = 9;
+	const board = new Array(boardSize);
+
+	const showBoard = () => {
+		console.log(board);
+	}
+
+	const setBoard = () => {
+		for (let i = 0; i < boardSize; i++) {
+			board[i] = false;
+		}
+	}
+
+	setBoard();
+	return {
+		showBoard,
+	}
+})();
+
+gameBoard.showBoard();
